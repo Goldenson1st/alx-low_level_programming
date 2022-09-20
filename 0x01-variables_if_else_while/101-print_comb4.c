b8bdc0647d3bf6a2836a1ea2bfd0a6cd82ab36c7
Bello Usman <goldenson1st@gmail.com>
2022-09-19 21:11:01 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,19 +13,19 @@
  	int i, j, k, l;
 
 	i = 0;
-	while (i < 1000);
+	while (i < 1000)
 	{
 	j = i / 100;
 	k = (i / 10) % 10;
 	l = i % 10;
 
-	if (j < k && k < l);
+	if (j < k && k < l)
 	{
 	putchar(j + '0');
 	putchar(k + '0');
 	putchar(l + '0');
 
-	if (i < 700);
+	if (i < 700)
 	{
 	putchar(44);
 	putchar(32);
